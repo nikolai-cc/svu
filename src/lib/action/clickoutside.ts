7f21091500c82ba88cd0a052e7973297ae51ed41
nikolai-cc <nikolai@theministry.co>
2022-05-25 11:20:50 +0200

    action/clickoutside

---
+++ src/lib/action/clickoutside.ts
@@ -0,0 +1,20 @@
+import { listen } from '$lib/meta'
+
+/**
+ * Svelte action to execute a function when clicking anywhere but on the target node.
+ * Also dispatches the `clickoutside` event on the target node.
+ * Usage: <element use:clickoutside={ handler() } />
+ */
+export const clickoutside = (node: HTMLElement, handler?: Function) => {
+    const handleClick = (event: PointerEvent) => {
+        if (node.contains(event.target as HTMLElement)) return
+        handler && handler()
+        node.dispatchEvent(new CustomEvent('clickoutside'));
+    }
+    const unlisten = listen(document, 'click', handleClick as EventListenerOrEventListenerObject)
+
+    return {
+        update: noop,
+        destroy: unlisten
+    }
+}
