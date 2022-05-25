3c630f2a8bf963f75f3ae0819bfa336e8058c272
nikolai-cc <nikolai@theministry.co>
2022-05-25 11:23:28 +0200

    meta/clone

---
+++ src/lib/meta/clone.ts
@@ -0,0 +1,42 @@
+/**
+ * Function that deep clones any object up to a maximum recursion depth.
+ */
+export const clone = (obj: any, depth: number = Infinity) => {
+    let parents: any[] = [];
+    let children: any[] = [];
+
+    const _clone = (obj: any, depth: number) => {
+        if (typeof(obj) !== 'object' || depth === 0) { return obj }
+        
+        let child: any;
+        if (obj instanceof Map) { child = new Map() }
+        else if (obj instanceof Set) { child = new Set() }
+        else if (obj instanceof Date) { child = new Date(obj.getTime()) }
+        else if (obj instanceof Array) { child = [] }
+        else if (obj instanceof Error) { child = Object.create(obj) }
+        else if (obj instanceof RegExp) { child = new RegExp(obj.source, obj.flags) }
+        else if (obj instanceof Promise) { child = new Promise(
+            (res, rej) => obj.then(
+                v => res(_clone(v, depth - 1)),
+                e => rej(_clone(e, depth - 1))
+            ))
+        } else { child = Object.create(Object.getPrototypeOf(obj)) }
+
+        if (parents.includes(obj)) { return children[parents.indexOf(obj)] }
+        parents.push(obj);
+        children.push(child);
+
+        if (obj instanceof Map) {
+            for (let [k, v] of obj) { child.set(k, _clone(v, depth - 1)) }
+        }
+        else if (obj instanceof Set) {
+            for (let v of obj) { child.add(_clone(v, depth - 1)) }
+        }
+        else {
+            for (let key in obj) { child[key] = _clone(obj[key], depth - 1) }
+        }
+        return child;
+    }
+
+    return _clone(obj, depth);
+}
