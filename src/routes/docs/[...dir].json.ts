a1884158d809f8d297e49f45eac99c0a9536990f
nikolai-cc <nikolai@theministry.co>
2022-07-08 17:57:26 +0200

    remove log statement

--- src/routes/docs/[...dir].json.ts
+++ src/routes/docs/[...dir].json.ts
@@ -3,7 +3,6 @@ import type { RequestEvent } from '@sveltejs/kit';
 
 export const get = async ({ params }: RequestEvent) => {
 	const dir = params.dir.split('/')[0];
-	console.log(dir)
 	const chapters = await ls(`./src/routes/docs/${dir}/`, 'dirs');
 
 	return {
