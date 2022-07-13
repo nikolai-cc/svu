d984f363eeab486f8086e05b4e7cc38779988d73
nikolai-cc <nikolai@theministry.co>
2022-07-13 15:02:58 +0200

    add Article component

---
+++ src/routes/_docs/Article.svelte
@@ -0,0 +1,19 @@
+<article class="flow">
+	<slot />
+</article>
+
+<style>
+	article {
+		max-height: 100vh;
+		max-width: 1200px;
+		margin-left: auto;
+		margin-right: auto;
+		padding-left: 2rem;
+		padding-right: 2rem;
+		padding-top: 3rem;
+		padding-bottom: 3rem;
+		width: 100%;
+		overflow-y: auto;
+		overflow-x: hidden;
+	}
+</style>
