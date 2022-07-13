b68efef981b384d2fbc2ace7e681d92251c6af27
nikolai-cc <nikolai@theministry.co>
2022-07-13 10:51:40 +0200

    allow passing in target by queryselector

--- src/lib/action/clipboard.ts
+++ src/lib/action/clipboard.ts
@@ -4,8 +4,8 @@ import { listen } from "$lib/meta"
  * Copies the textContent of the element (or a specified target) to clipboard on click.
  * Usage: <element use:copy={ target } />
  */
-export const copy = (node: HTMLElement, target?: HTMLElement) => {
-    let object = target ?? node;
+export const copy = (node: HTMLElement, target?: HTMLElement | string) => {
+    let object = (typeof target === 'string' ? document.querySelector(target) : target) ?? node;
     const copyObject = () => {
         let text = (object instanceof HTMLInputElement) ? object.value : object.textContent || '';
         navigator.clipboard.writeText(text)
@@ -14,7 +14,7 @@ export const copy = (node: HTMLElement, target?: HTMLElement) => {
     const unlisten = listen(node, 'click', copyObject);
 
     return {
-        update: (target: HTMLElement) => object = target ?? node,
+        update: (target: HTMLElement | string) => (typeof target === 'string' ? document.querySelector(target) : target) ?? node,
         destroy: unlisten
     }
 }
@@ -23,8 +23,8 @@ export const copy = (node: HTMLElement, target?: HTMLElement) => {
  * Pastes the textContent of the clipboard into the textContent of the element (or a specified target)  on click.
  * Usage: <element use:paste={ target } />
  */
-export const paste = (node: HTMLElement, target?: HTMLElement) => {
-    let object = target ?? node;
+export const paste = (node: HTMLElement, target?: HTMLElement | string) => {
+    let object = (typeof target === 'string' ? document.querySelector(target) : target) ?? node;
 
     const pasteObject = async () => { 
         const text = await navigator.clipboard.readText();
@@ -35,7 +35,7 @@ export const paste = (node: HTMLElement, target?: HTMLElement) => {
     const unlisten = listen(node, 'click', pasteObject);
 
     return {
-        update: (target: HTMLElement) => object = target ?? node,
+        update: (target: HTMLElement | string) => (typeof target === 'string' ? document.querySelector(target) : target) ?? node,
         destroy: unlisten
     }
 }
