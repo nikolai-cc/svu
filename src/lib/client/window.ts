00ecf4f2241b9c3885aa6af16df51517981e4571
nikolai-cc <nikolai@theministry.co>
2022-07-13 23:05:48 +0200

    add stores for mx my, sx, sy

--- src/lib/client/window.ts
+++ src/lib/client/window.ts
@@ -23,17 +23,31 @@ export const wh = createSize('innerHeight');
 /**
  * Svelte store that tracks the window size.
  */
-export const windowSize = derived([ww, wh], ([$ww, $wh]) => ({ w: $ww, h: $wh }));
+export const windowSize = derived(
+    [ww, wh], ([$ww, $wh]) => ({ w: $ww, h: $wh })
+);
 
-const createScroll = () => {
-    const { subscribe, set } = writable({ x: 0, y: 0 });
+const createScroll = (prop: 'scrollX' | 'scrollY') => {
+    const { subscribe, set } = writable(0);
     if (!browser) return { subscribe };
-    set({ x: window.scrollX, y: window.scrollY })
-    listen(window, 'scroll', () => set({ x: window.scrollX, y: window.scrollY }));
+    set(window[prop])
+    listen(window, 'scroll', () => set(window[prop]));
     return { subscribe }
 }
 
 /**
+ * Svelte store that tracks the scroll x position.
+ */
+export const sx = createScroll('scrollX');
+
+/**
+ * Svelte store that tracks the scroll y position.
+ */
+export const sy = createScroll('scrollY');
+
+/**
  * Svelte store that tracks the scroll position.
  */
-export const scroll = createScroll();
+export const scroll = derived(
+    [sx, sy], ([$sx, $sy]) => ({ x: $sx, y: $sy })
+);
