3681d32f4fdf2428a32d02f45383d6ca83f29f83
nikolai-cc <nikolai@theministry.co>
2022-07-14 15:54:27 +0200

    add typewriter transition

---
+++ src/lib/transition/typewriter.ts
@@ -0,0 +1,22 @@
+/**
+ * Simple typewriter transition. Based on the svelte tutorial typewriter transition.
+ */
+export const typewriter = (node: Element, options: { speed?: number, delay?: number }) => {
+    const { speed = 100, delay = 0 } = options ?? {};
+
+	const valid =
+		node.childNodes.length === 1 && node.childNodes[0].nodeType === 3;
+	if (!valid) return {};
+
+	const text = node.textContent || "";
+	const duration = text.length * speed;
+
+	return {
+		duration,
+		delay,
+		tick: (t: number, u: number) => {
+			const i = Math.trunc(text.length * t);
+			node.textContent = text.slice(0, i);
+		}
+	};
+}
