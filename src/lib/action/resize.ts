f95619ffab483fdbba6e1311cbec34ec181a33f1
nikolai-cc <nikolai@theministry.co>
2022-07-14 15:54:05 +0200

    add use:resize action

---
+++ src/lib/action/resize.ts
@@ -0,0 +1,28 @@
+import { listen, noop } from "$lib/meta"
+
+/**
+ * Dispatches an event or calls a handler if an element is resized.
+ * Usage: <element use:resize={{ handler: () => console.log('hello') }} />
+ * For tracking <window> size it's best to use the window store from svutil/client.
+*/
+export const resize = (node: HTMLElement, options?: { handler?: Function }) => {
+    let handler = options?.handler || noop
+
+    const handleResize = (entries: ResizeObserverEntry[]) => {
+        entries.forEach(e => {
+            if (e.target !== node) return
+            handler()
+            node.dispatchEvent(new CustomEvent('resize', { detail: e.target }))
+        })
+    }
+    
+    let observer = new ResizeObserver(handleResize);
+    observer.observe(node);
+
+    return {
+        update: (options?: { handler?: Function }) => {
+            handler = options?.handler || noop
+        },
+        destroy: () => observer.disconnect(),
+    }
+}
