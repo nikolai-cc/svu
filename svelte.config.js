4ce9ece41dd5b38d2480ae7a964bde01b3887aff
nikolai-cc <nikolai@theministry.co>
2022-07-14 22:40:49 +0200

    set adapter-auto for docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
