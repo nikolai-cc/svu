9ac3bd215e6f8c055044ad58e3e1765104db792d
nikolai-cc <nikolai@theministry.co>
2022-07-18 00:18:52 +0200

    add typedclick types

---
+++ src/lib/action/timedclick.d.ts
@@ -0,0 +1,7 @@
+declare namespace svelte.JSX {
+    interface HTMLProps<T> {
+      ontimedclick?: (event: CustomEvent<{}>) => void;
+      'ontimedclick:armed'?: (event: CustomEvent<{}>) => void;
+      'ontimedclick:aborted'?: (event: CustomEvent<{}>) => void;
+    }
+  }
