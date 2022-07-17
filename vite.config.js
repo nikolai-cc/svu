fd199bc5fff3cc10497f7f5ce680879208476f29
nikolai-cc <nikolai@theministry.co>
2022-07-17 17:20:54 +0200

    add optimizedeps & nossr fields

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		exclude: ['svu']
+	},
+	ssr: {
+		noExternal: ['svu']
+	}
 };
 
 export default config;
