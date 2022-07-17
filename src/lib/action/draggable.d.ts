2d19c40933fa103dfe69b3f6b8835c3b14837fe2
nikolai-cc <nikolai@theministry.co>
2022-07-18 00:13:45 +0200

    add draggable event types

---
+++ src/lib/action/draggable.d.ts
@@ -0,0 +1,7 @@
+declare namespace svelte.JSX {
+    interface HTMLProps<T> {
+      'ondrag:start'?: (event: CustomEvent<{}>) => void,
+      'ondrag:update'?: (event: CustomEvent<{}>) => void,
+      'ondrag:end'?: (event: CustomEvent<{}>) => void
+    }
+  }
