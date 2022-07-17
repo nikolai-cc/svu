dad6794a93369ab0cad557237cef43b6be335880
nikolai-cc <nikolai@theministry.co>
2022-07-18 00:20:10 +0200

    add viewport types

---
+++ src/lib/action/viewport.d.ts
@@ -0,0 +1,6 @@
+declare namespace svelte.JSX {
+    interface HTMLProps<T> {
+      'onviewport:enter'?: (event: CustomEvent<{}>) => void;
+      'onviewport:leave'?: (event: CustomEvent<{}>) => void;
+    }
+  }
