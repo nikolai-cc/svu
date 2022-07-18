a30d33396687c7a54da2ff6097e131f71f552bbc
nikolai-cc <nikolai@theministry.co>
2022-07-18 23:31:24 +0200

    add some clarity to ts docstrings

--- src/lib/client/media.ts
+++ src/lib/client/media.ts
@@ -1,76 +1,76 @@
 import { mediaquery } from "../store/mediaquery.js";
 
 /**
- * Is true if `prefers-reduced-motion` is set to `no-preference`.
+ * Is true when `prefers-reduced-motion` is set to `no-preference`.
  */
 export const motionOK = mediaquery('prefers-reduced-motion', 'no-preference');
 
 /**
- * Is true if `prefers-reduced-motion` is set to `reduce`.
+ * Is true when `prefers-reduced-motion` is set to `reduce`.
  */
 export const reduceMotion = mediaquery('prefers-reduced-motion', 'reduce');
 
 /**
- * Is true if the page is previewed for printing.
+ * Is true when the page is (pre)viewed for printing.
  */
 export const print = mediaquery('print');
 
 /**
- * Is true if the page is viewed on a screen;
+ * Is true when the page is viewed on a screen;
  */
 export const screen = mediaquery('screen');
 
 /**
- * Is true if the page is wider than it is tall;
+ * Is true when the viewport is wider than it is tall;
  */
 export const landscape = mediaquery('orientation', 'landscape');
 
 /**
- * Is true if the page is taller than it is wide;
+ * Is true when the viewport is taller than it is wide;
  */
  export const portrait = mediaquery('orientation', 'portrait');
 
 /**
- * Is true if the page allows hovering;
- */
- export const hover = mediaquery('hover', 'hover');
-
-/**
- * Is true if the client prefers a dark color scheme.
+ * Is true when the client prefers a dark color scheme.
  */
 export const dark = mediaquery('prefers-color-scheme', 'dark');
 
 /**
- * Is true if the client prefers light color scheme.
+ * Is true when the client prefers light color scheme.
  */
 export const light = mediaquery('prefers-color-scheme', 'light');
 
 /**
- * Is true if the client prefers a high contrast color scheme.
+ * Is true when the client prefers a high contrast color scheme.
  */
-export const highContrast = mediaquery('prefers-color-scheme', 'more');
+export const highContrast = mediaquery('prefers-contrast', 'more');
 
 /**
- * Is true if the client prefers a low contrast color scheme.
+ * Is true when the client prefers a low contrast color scheme.
  */
- export const lowContrast = mediaquery('prefers-color-scheme', 'less');
+ export const lowContrast = mediaquery('prefers-contrast', 'less');
 
  /**
- * Is true if the has no contrast preference.
+ * Is true when the has no contrast preference.
  */
-export const defaultContrast = mediaquery('prefers-color-scheme', 'no-preference');
+export const defaultContrast = mediaquery('prefers-contrast', 'no-preference');
 
 /**
- * Is true if the client does not have a pointer available.
+ * Is true when the device can display a hover state. (You can do so with a mouse, but not with a finger + touchscreen.)
+ */
+ export const hover = mediaquery('hover', 'hover');
+
+/**
+ * Is true when the client does not have a pointer available.
  */
 export const noPointer = mediaquery('pointer', 'none');
 
 /**
- * Is true if the client has a coarse pointer available.
+ * Is true when the client has a coarse pointer available.
  */
 export const coarsePointer = mediaquery('pointer', 'coarse');
 
 /**
- * Is true if the client has a fine pointer available.
+ * Is true when the client has a fine pointer available.
  */
 export const finePointer = mediaquery('pointer', 'fine');
