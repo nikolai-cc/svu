61c1747aff93ae0178f939ee188dbf1a7f161b37
Mathias Picker <mattis123@live.no>
2022-07-22 01:23:18 +0200

    reverted to old clone method

--- src/lib/meta/clone.ts
+++ src/lib/meta/clone.ts
@@ -3,35 +3,60 @@
  */
 export const clone = typeof structuredClone === 'function' ? structuredClone : _clone;
 
-// copied from svelte codebase:
-// adapted from klona v2.0.4 - https://github.com/lukeed/klona
-// (c) Luke Edwards, under MIT License
-function _clone(val: any) {
-	let k, out, tmp;
+function _clone(obj: any) {
+	const parents: any[] = [];
+	const children: any[] = [];
 
-	if (Array.isArray(val)) {
-		out = Array((k = val.length));
-		while (k--) out[k] = (tmp = val[k]) && typeof tmp === 'object' ? clone(tmp) : tmp;
-		return out;
-	}
+	const __clone = (obj: any) => {
+		if (typeof obj !== 'object') {
+			return obj;
+		}
 
-	if (Object.prototype.toString.call(val) === '[object Object]') {
-		out = {}; // null
-		for (k in val) {
-			if (k === '__proto__') {
-				Object.defineProperty(out, k, {
-					value: clone(val[k]),
-					configurable: true,
-					enumerable: true,
-					writable: true
-				});
-			} else if (typeof val[k] !== 'function') {
-				// MODIFICATION: skip functions
-				out[k] = (tmp = val[k]) && typeof tmp === 'object' ? clone(tmp) : tmp;
+		let child: any;
+		if (Array.isArray(obj)) {
+			child = [];
+		} else if (obj instanceof Map) {
+			child = new Map();
+		} else if (obj instanceof Set) {
+			child = new Set();
+		} else if (obj instanceof Date) {
+			child = new Date(obj.getTime());
+		} else if (obj instanceof Error) {
+			child = Object.create(obj);
+		} else if (obj instanceof RegExp) {
+			child = new RegExp(obj.source, obj.flags);
+		} else if (obj instanceof Promise) {
+			child = new Promise((res, rej) =>
+				obj.then(
+					(v) => res(__clone(v)),
+					(e) => rej(__clone(e))
+				)
+			);
+		} else {
+			child = Object.create(Object.getPrototypeOf(obj));
+		}
+
+		if (parents.includes(obj)) {
+			return children[parents.indexOf(obj)];
+		}
+		parents.push(obj);
+		children.push(child);
+
+		if (obj instanceof Map) {
+			for (let [k, v] of obj) {
+				child.set(k, __clone(v));
+			}
+		} else if (obj instanceof Set) {
+			for (let v of obj) {
+				child.add(__clone(v));
+			}
+		} else {
+			for (let key in obj) {
+				child[key] = __clone(obj[key]);
 			}
 		}
-		return out;
-	}
+		return child;
+	};
 
-	return val;
+	return __clone(obj);
 }
