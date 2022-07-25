061b11b08136960cd8ed265f78bcd25d82acec94
nikolai-cc <nikolai@theministry.co>
2022-07-25 11:16:25 +0200

    fix unresolved merge conflict

--- src/lib/action/resize.ts
+++ src/lib/action/resize.ts
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-import { listen, noop } from '../meta';
-=======
-import { noop } from '$lib/meta/index.js';
->>>>>>> 43a2b44386f095009ed548cbc86c5283b604b0fd
+import { noop } from '../meta';
 
 /**
  * Dispatches an event or calls a handler if an element is resized.
  * Usage: <element use:resize={{ handler: () => console.log('hello') }} />
  * For tracking <window> size it's best to use the window store from svu/client.
  */
-<<<<<<< HEAD
-export const resize = (node: HTMLElement, options?: { handler?: Function }) => {
-=======
 export const resize = (node: HTMLElement, options?: { handler?: (...params: any) => any }) => {
->>>>>>> 43a2b44386f095009ed548cbc86c5283b604b0fd
 	let handler = options?.handler || noop;
 
 	const handleResize = (entries: ResizeObserverEntry[]) => {
@@ -24,19 +16,11 @@ export const resize = (node: HTMLElement, options?: { handler?: (...params: any)
 		});
 	};
 
-<<<<<<< HEAD
-	let observer = new ResizeObserver(handleResize);
-	observer.observe(node);
-
-	return {
-		update: (options?: { handler?: Function }) => {
-=======
 	const observer = new ResizeObserver(handleResize);
 	observer.observe(node);
 
 	return {
 		update: (options?: { handler?: (...params: any) => any }) => {
->>>>>>> 43a2b44386f095009ed548cbc86c5283b604b0fd
 			handler = options?.handler || noop;
 		},
 		destroy: () => observer.disconnect()
