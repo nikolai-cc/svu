24a6cd328634dd11ba0198797f6bd90681909696
nikolai-cc <nikolai@theministry.co>
2022-07-25 15:43:32 +0200

    vitest: use include over exclude pattern

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		globals: true,
-		exclude: [
-			'**/*.spec.ts',
-			'**/node_modules/**',
-			'**/.svelte-kit/**',
-			'**/.{idea,git,cache,output,temp}/**'
-		]
+		include: ["test/**/*.test.ts"],
 	}
 });
