39684b197f162070c9e0bb3cdcab11d83a9b1173
nikolai-cc <nikolai@theministry.co>
2022-07-25 23:02:42 +0200

    remove superfluous test and use fs.existsSync

--- test/setup.ts
+++ test/setup.ts
@@ -1,28 +1,13 @@
 import fs from 'node:fs';
 import { exec } from 'node:child_process';
 import { promisify } from 'node:util';
-import fg from 'fast-glob';
-import { compile } from 'svelte/compiler';
 
 /**
  * This script sets up tests for playwright
  */
 
 // Making sure .svelte-kit/output is present (needed for playwright to work)
-if (!(fs.readdirSync('./.svelte-kit/output')?.length > 0)) {
+if (!(fs.existsSync('./.svelte-kit/output'))) {
 	const run = promisify(exec);
 	await run('npm run build');
-}
-
-// Compiling all svelte components in test dir
-const svelte_components = await fg('./test/**/*/*.svelte');
-
-svelte_components.forEach(compile_svelte_component);
-
-function compile_svelte_component(component_path: string) {
-	const source = fs.readFileSync(component_path, 'utf-8');
-	const { js } = compile(source);
-	const new_path = component_path.replace('.svelte', '.svelte.js');
-	fs.writeFileSync(new_path, js.code);
-	return new_path;
 }
