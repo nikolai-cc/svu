619dc8c67c667b7f7f73c8e09a63e6327220c4cd
nikolai-cc <nikolai@theministry.co>
2022-08-05 15:15:51 +0200

    add test component

---
+++ src/lib/action/active/Active.test.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { active } from './active';
+	import { page } from '$app/stores';
+
+	console.log($page.url.href);
+	console.log(document.location.href);
+</script>
+
+<nav>
+	<a href="/" use:active>Active</a>
+	<a href="/inactive" use:active>Inactive</a>
+</nav>
