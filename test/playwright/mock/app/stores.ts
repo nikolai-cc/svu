a37362f15b7be0831300deaeb4092515a5a801c0
nikolai-cc <nikolai@theministry.co>
2022-08-05 15:44:10 +0200

    add $app mock

---
+++ test/playwright/mock/app/stores.ts
@@ -0,0 +1,10 @@
+import { readable, writable } from 'svelte/store';
+
+export const page = writable({
+    error: null,
+    params: {},
+    routeId: '',
+    status: 200,
+    stuff: {},
+    url: new URL('http://svu.dev')
+})
