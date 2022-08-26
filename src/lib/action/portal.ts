5342352bf95c2ff6646e6346bfc32990986ba80e
nikolai-cc <nikolai@theministry.co>
2022-08-26 11:44:14 +0200

    fix broken portal

--- src/lib/action/portal.ts
+++ src/lib/action/portal.ts
@@ -1,10 +1,10 @@
 /**
  * Mounts a component elsewhere in the DOM.
  * Pass in the target parent by reference or selector.
- * Usage: <element use:portal={ target: 'main' } />
+ * Usage: <element use:portal={'#target'} /> (as selector) or <element use:portal={target} /> (as HTMLelement)
  */
 export const portal = (node: HTMLElement, target: HTMLElement | string) => {
-	typeof target === 'string' && document.querySelector(target)?.appendChild(node);
+	typeof target === 'string' ? document.querySelector(target)?.appendChild(node) : target.appendChild(node);
 
 	return {
 		update: (target: HTMLElement | string) => {
