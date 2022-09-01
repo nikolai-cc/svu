5429d056fe5b00674131494703214e033128c435
nikolai-cc <nikolai@theministry.co>
2022-09-01 16:23:56 +0200

    fix portal when passing in HTMLElement

--- src/lib/action/portal.ts
+++ src/lib/action/portal.ts
@@ -1,16 +1,16 @@
 /**
  * Mounts a component elsewhere in the DOM.
  * Pass in the target parent by reference or selector.
- * Usage: <element use:portal={'#target'} /> (as selector) or <element use:portal={target} /> (as HTMLelement)
+ * Usage: <element use:portal={'#target'} /> (as querySelector) or <element use:portal={target} /> (as HTMLelement)
  */
 export const portal = (node: HTMLElement, target: HTMLElement | string) => {
-	typeof target === 'string' ? document.querySelector(target)?.appendChild(node) : target.appendChild(node);
-
+	let targetElement = typeof target === 'string' ? document.querySelector(target) : target;
+	targetElement && targetElement.appendChild(node)
+	
 	return {
 		update: (target: HTMLElement | string) => {
-			typeof target === 'string'
-				? document.querySelector(target)?.appendChild(node)
-				: target.appendChild(node);
+			let targetElement = typeof target === 'string' ? document.querySelector(target) : target;
+			targetElement && targetElement.appendChild(node)
 		},
 		destroy: () => node.parentElement?.removeChild(node)
 	};
