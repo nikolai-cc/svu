64206a9960f4cbf42abc3dfb82c728b90ab9ec0f
nikolai-cc <nikolai@theministry.co>
2022-09-02 16:25:24 +0200

    decouple env.dev from SvelteKit

--- src/lib/meta/env.ts
+++ src/lib/meta/env.ts
@@ -1,2 +1,21 @@
-// Returns true in browser, false when prerendering / running in node.
+/** Returns true in browser, false when prerendering / running in node. */
 export const browser = typeof window !== 'undefined' && window.document;
+
+declare const __SVELTEKIT_DEV__: boolean |undefined
+declare const __SVU_DEV__: boolean |undefined
+
+const checkDev = () => {
+    try {
+        return __SVU_DEV__
+        // return __SVELTEKIT_DEV__ || __SVU_DEV__
+    } catch (e) {
+        return false
+    }
+}
+
+/**
+ * Returns true in development mode, false in production. This works automagically when using SvelteKit.
+ * When not using SvelteKit, you can set the __SVU_DEV__ global variable to true in dev mode.
+ * Using vite, you can use [config.define](https://vitejs.dev/config/shared-options.html#define) to achieve this, your .
+ */
+export const dev = checkDev();
