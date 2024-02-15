dd182fe3a1cb5db3c58332a4bd328596873bfd2c
nikolai <nikolai@theministry.co>
2024-02-15 18:06:54 +0100

    add initial use:draggable action

---
+++ src/lib/action/draggable.ts
@@ -0,0 +1,126 @@
+import { listen, getElement, getDomRect, clamp, type ElementOrSelector } from '$lib/meta/index.js';
+import type { Coords } from '$lib/meta/types.js';
+
+export interface UseDraggableOptions {
+	position?: Coords;
+	handle?: ElementOrSelector;
+	axis?: 'x' | 'y' | 'xy';
+	container?: ElementOrSelector;
+	class?: string;
+}
+
+/**
+ * Returns the minimum and maximum bounds for the draggable element based on an optional container element.
+ */
+function setBounds(node: HTMLElement, container: HTMLElement | undefined) {
+	const min = { x: 0, y: 0 };
+	const max = { x: Infinity, y: Infinity };
+
+	if (container) {
+		const containerRect = getDomRect(container);
+		const nodeRect = getDomRect(node);
+
+		min.x = containerRect.left - nodeRect.left;
+		min.y = containerRect.top - nodeRect.top;
+		max.x = containerRect.left + containerRect.width - nodeRect.left - nodeRect.width;
+		max.y = containerRect.top + containerRect.height - nodeRect.top - nodeRect.height;
+	}
+
+	return { min, max };
+}
+
+/**
+ * Allows positioning of an element by dragging it from the element or an optional handle defined with `options.handle`.
+ * Emits `!drag:start`, `!drag` and `!drag:end` events on the element. Update the `options.position` property to programmatically move the element.
+ * Use the `options.axis` property to limit movement to a single axis (`x` or `y`). Use the `options.container` property to limit movement to inside a container element.
+ * When dragging the dragged element gets `options.class` (`svu-dragging` by default). Use a 'scoped global' style to add component-specific styling (see example below).
+ *
+ * Example:
+ * ```svelte
+ * <div use:draggable={{ position: { x: 100, y: 100 }, handle: '#element' }} />
+ *
+ * <style>
+ *  div:global(.svu-dragging) { cursor: grabbing; }
+ * </style>
+ * ```
+ */
+export function draggable(node: HTMLElement, options?: UseDraggableOptions) {
+	let { position = { x: 0, y: 0 }, axis = 'xy' } = options || {};
+
+	let className = options?.class || 'svu-dragging';
+	let handle = getElement(options?.handle, node);
+	let container = getElement(options?.container);
+	let origin = position;
+
+	let { min, max } = setBounds(node, container);
+
+	function handlePointerDown(event: PointerEvent) {
+		event.preventDefault();
+		event.stopPropagation();
+		node.setPointerCapture(event.pointerId);
+		node.classList.add(className);
+
+		origin = {
+			x: event.clientX,
+			y: event.clientY
+		};
+
+		node.dispatchEvent(new CustomEvent('!dragstart', { detail: position }));
+
+		function handlePointerMove(event: PointerEvent) {
+			event.preventDefault();
+			event.stopPropagation();
+
+			position.x = axis.includes('x') ? position.x + event.clientX - origin.x : position.x;
+			position.y = axis.includes('y') ? position.y + event.clientY - origin.y : position.y;
+
+			position.x = clamp(position.x, min.x, max.x);
+			position.y = clamp(position.y, min.y, max.y);
+
+			draw();
+
+			origin = {
+				x: event.clientX,
+				y: event.clientY
+			};
+
+			node.dispatchEvent(new CustomEvent('!drag', { detail: position }));
+		}
+
+		function handlePointerUp(event: PointerEvent) {
+			event.preventDefault();
+			event.stopPropagation();
+			node.releasePointerCapture(event.pointerId);
+			node.classList.remove(className);
+			node.dispatchEvent(new CustomEvent('!dragend', { detail: position }));
+
+			unlistenPointerMove();
+			unlistenPointerUp();
+		}
+
+		const unlistenPointerMove = listen(window, 'pointermove', handlePointerMove as EventListener);
+		const unlistenPointerUp = listen(window, 'pointerup', handlePointerUp as EventListener);
+	}
+
+	function draw() {
+		node.style.transform = `translate(${position.x}px, ${position.y}px)`;
+	}
+
+	const unlistenPointerDown = listen(handle, 'pointerdown', handlePointerDown as EventListener);
+	const unlistenTouchStart = listen(handle, 'touchstart', (e) => e.preventDefault());
+
+	return {
+		update(options: UseDraggableOptions) {
+			position = options.position || position;
+			className = options.class || 'svu-dragging';
+			handle = getElement(options.handle, node);
+			axis = options.axis || axis;
+			container = getElement(options.container) || container;
+			({ min, max } = setBounds(node, container));
+		},
+		destroy() {
+			unlistenPointerDown();
+			unlistenTouchStart();
+		}
+	};
+}
--- src/lib/action/index.ts
+++ src/lib/action/index.ts
@@ -2,3 +2,4 @@ export { active } from './active.js';
 export { clickoutside } from './clickoutside.js';
 export { copy, cut, paste } from './clipboard.js';
 export { download } from './download.js';
+export { draggable } from './draggable.js';
