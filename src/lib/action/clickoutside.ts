8ed705ee0a8d6a1402358d5e68cbd5acd36d58a1
nikolai <nikolai@theministry.co>
2024-04-06 21:16:02 +0200

    fix clickoutside docstring

--- src/lib/action/clickoutside.ts
+++ src/lib/action/clickoutside.ts
@@ -9,12 +9,12 @@ type Handler = (event?: Event) => void;
 
 /**
  * Calls `handler` when a click event occurs outside of `node`. The event is forwarded to `handler`.
- * Also dispatches a `clickoutside` event on 'node' with the original event as `detail`.
+ * Also dispatches a `!clickoutside` event on 'node' with the original event as `detail`.
  *
  * Example:
  * ```svelte
  * <element use:clickoutside={handler} />
- * <element use:clickoutside on:clickoutside={handler} />
+ * <element use:clickoutside on:!clickoutside={handler} />
  * ```
  */
 export function clickoutside(
