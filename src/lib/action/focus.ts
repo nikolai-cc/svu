5ee283447b2995a2dad99021cc9ca7cbbae5f253
nikolai <nikolai@theministry.co>
2024-04-06 22:16:22 +0200

    add use:focus action

---
+++ src/lib/action/focus.ts
@@ -0,0 +1,14 @@
+import { noop } from '../meta/index.js';
+
+/**
+ * Focuses element when it mounts. Only works on focusable elements.
+ * Usage: <element use:focus />
+ */
+export const focus = (node: HTMLElement) => {
+	if (typeof node.focus === 'function') node.focus();
+
+	return {
+		update: noop,
+		destroy: noop
+	};
+};
