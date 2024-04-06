80bea88f4c712e39b748de022a014be7ac8eafce
nikolai <nikolai@theministry.co>
2024-04-07 00:58:29 +0200

    add use:themetoggle

---
+++ src/lib/action/themetoggle.ts
@@ -0,0 +1,33 @@
+import { listen, noop } from '../meta/index.js';
+
+/**
+ * Theme Toggler.
+ * Sets the 'data-theme' attribute on the html to a theme or the next of a list of themes on click.
+ * Usage: <element use:theme={['dark', 'light']} /> toggles between dark and light
+ * Usage: <element use:theme={'dark'} /> sets theme to dark
+ */
+export function themetoggle(node: HTMLElement, themes?: string | string[]) {
+	themes = themes ?? ['light', 'dark'];
+	const target = document.documentElement;
+	let theme = 0;
+
+	function toggle(themes: string[]) {
+		theme = (theme + 1) % themes.length;
+		target.setAttribute('data-theme', themes[theme]);
+	}
+
+	function set(themes: string) {
+		target.setAttribute('data-theme', themes);
+	}
+
+	const unlisten = listen(
+		node,
+		'click',
+		typeof themes === 'string' ? () => set(<string>themes) : () => toggle(<string[]>themes)
+	);
+
+	return {
+		update: noop,
+		destroy: unlisten
+	};
+}
