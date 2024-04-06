df0f1aeec5d127d92fae48d9fb2ae1ae8103c054
nikolai <nikolai@theministry.co>
2024-04-06 21:52:37 +0200

    fix clipboard event types

--- src/lib/action/clipboard.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace svelteHTML {
-	interface HTMLAttributes {
-		'on:!copy'?: (event: CustomEvent<string>) => void;
-		'on:!cut'?: (event: CustomEvent<string>) => void;
-		'on:!paste'?: (event: CustomEvent<string>) => void;
-	}
-}
