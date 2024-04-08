f0a2c34eda6e9294bf857bd690189a0b3ee774eb
nikolai <nikolai@theministry.co>
2024-04-08 14:14:55 +0200

    add wait fn

--- src/lib/meta/time.ts
+++ src/lib/meta/time.ts
@@ -3,7 +3,14 @@ import type { Fn } from './fn.js';
 /**
  * Set a timeout to execute a function. Returns a function that clears the timeout.
  */
-export function timeout(handler: Fn, delay: number, ...args: unknown[]) {
-	const timer = setTimeout(handler, delay, ...args);
+export function timeout(handler: Fn, delay_ms: number, ...args: unknown[]) {
+	const timer = setTimeout(handler, delay_ms, ...args);
 	return () => clearTimeout(timer);
+}
+
+/**
+ * Lets you asynchronously wait for a given time.
+ */
+export function wait(delay_ms: number): Promise<void> {
+	return new Promise((resolve) => setTimeout(resolve, delay_ms));
 }
