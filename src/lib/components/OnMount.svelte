6172b40ba4637ae8f714191373bc7c6bbc9fdc6b
nikolai <nikolai@theministry.co>
2024-04-09 15:46:50 +0200

    onmount component

---
+++ src/lib/components/OnMount.svelte
@@ -0,0 +1,14 @@
+<!--@component
+    OnMount component. Renders any children when the page is loaded. Allows to bind to state to check status outside of component.
+-->
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	export let mounted = false;
+
+	onMount(() => (mounted = true));
+	onDestroy(() => (mounted = false));
+</script>
+
+{#if mounted}
+	<slot />
+{/if}
