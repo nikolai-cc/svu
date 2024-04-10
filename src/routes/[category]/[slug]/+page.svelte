f90d0447acbe37cce8c428f6c1525a2a44ab0dba
nikolai <nikolai@theministry.co>
2024-04-10 17:34:30 +0200

    scaffold doc pages

---
+++ src/routes/[category]/[slug]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { marked } from 'marked';
+
+	export let data;
+
+	$: content = marked(data.doc.content);
+</script>
+
+<article>
+	{@html content}
+</article>
+
+<style>
+	article {
+		padding: 1rem;
+	}
+</style>
