55aec232c46fe0f0e05ef604a11bd8512d3d6149
nikolai <nikolai@theministry.co>
2024-04-11 12:36:00 +0200

    frontpage scaffold

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,25 @@
 <script lang="ts">
+	export let data;
+
+	$: categories = data.categories;
 </script>
 
-<p>Hello, wolrd!</p>
+<article>
+	<h1>/SVU</h1>
+	<p>Svelte development, supercharged.</p>
+
+	<p>Choose a Category:</p>
+	<ul>
+		{#each categories as category}
+			<li>
+				<a href="/{category}">{category}</a>
+			</li>
+		{/each}
+	</ul>
+</article>
+
+<style>
+	article {
+		padding: 1rem;
+	}
+</style>
