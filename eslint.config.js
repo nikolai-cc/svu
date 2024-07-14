5ac96d5b22e8d79e60b8967b3590e8cd0c8d50cd
nikolai <nikolai@theministry.co>
2024-07-14 21:46:17 +0200

    config tweak

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,11 @@ export default [
 		rules: {
 			'no-duplicate-imports': 'off', // Allows importing types and code separately
 			'svelte/no-at-html-tags': 'off', // For markdown
-			'no-undef': 'off' // Seems to error with $stores and use:actions in .svelte files
+			'no-undef': 'off', // Seems to error with $stores and use:actions in .svelte files
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+			]
 		}
 	},
 	{
